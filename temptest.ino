477d2ed6c43b02a534a5bc8c204ca18eb0cff694
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-06-20 17:05:47 -0700

    Added Test Files and LCD functionality

--- temptest.ino
+++ temptest.ino
@@ -31,3 +31,25 @@ void loop() {
   digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
   }                 // wait for a second
 }
+
+float getTemperature(float voltage)
+{
+    if (Celsius == true)
+    {
+        return ((voltage - 0.5) * 100.0);
+    }
+    else
+    {
+        return ((voltage - 0.5) * 100.0) * (9.0 / 5.0) + 32.0;
+    }
+}
+
+float getVoltage(int pin)
+{
+
+    return (analogRead(pin) * 0.004882814);
+
+    // This equation converts the 0 to 1023 value that analogRead()
+    // returns, into a 0.0 to 5.0 value that is the true voltage
+    // being read at that pin.
+}
